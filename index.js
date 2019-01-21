0b7836a98df0d3618646a014b19d9ed20476c2a3
En <En>
2019-01-21 12:46:52 +0800

    FileReader Promise library init.

---
+++ index.js
@@ -0,0 +1,55 @@
+const defaultConfig = {
+  // FileReaderPromise readAsText return event.target.result or event
+  enableSimplify: true,
+}
+const config = Object.assign(defaultConfig)
+
+function getPromisifyReadFn (name) {
+  return function (blob) {
+    const argumentsOuter = arguments
+    const enableSimplify = config.enableSimplify
+
+    return new Promise(function (resolve, reject) {
+      const reader = new FileReader()
+
+      reader.onloadend = function (event) {
+        const result = enableSimplify ? event.target.result : event
+        // console.log(result)
+        resolve(result)
+      }
+      reader.onerror = function (error) {
+        reject(error)
+      }
+
+      reader[name].apply(reader, argumentsOuter)
+    })
+  }
+}
+
+const FileReaderPromise = {
+  set (newConfig) {
+    Object.assign(config, newConfig)
+  },
+}
+
+function isFunction (value) {
+  return typeof value === 'function'
+}
+
+function addMethods () {
+  const reader = new FileReader()
+
+  // readAsArrayBuffer()/readAsText()/...
+  for (let prop in reader) {
+    if (!reader.hasOwnProperty(prop) && isFunction(reader[prop])) {
+      FileReaderPromise[prop] = getPromisifyReadFn(prop)
+    }
+  }
+}
+
+// suppoted SSR for next.js/nuxt.js
+if (typeof FileReader === 'function') {
+  addMethods()
+}
+
+export default FileReaderPromise
